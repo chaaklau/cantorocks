96087bbf8ea3a6e87e13a57f46669b1bffd08a7c
Chaaak <chaakming@gmail.com>
2016-07-05 01:46:35 +0800

    changed config

--- gameconfig.js
+++ gameconfig.js
@@ -1,6 +1,6 @@
-var VELOCITIES = [1,1.5,2,4,7,20,20]; 
-//var LEVELBOUND = [0,32,64,128,256,512,768,9999999];
-var LEVELBOUND = [0,0,0,0,1,2,9999999];
+var VELOCITIES = [1,1.5,2,4,7,12,20]; 
+var LEVELBOUND = [0,32,64,128,256,512,768,9999999];
+//var LEVELBOUND = [0,0,0,0,1,2,9999999];
 
 function getVelocity(level){
   try {
