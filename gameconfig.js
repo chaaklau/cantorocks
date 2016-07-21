07974cfef415e0cc933a0105d19b61dca06f2381
Chaaak <chaakming@gmail.com>
2016-07-21 16:00:15 +0800

    improved parameters

--- gameconfig.js
+++ gameconfig.js
@@ -8,7 +8,7 @@
  */
 var MODE_NAMES = ["Normal", "Full", "Initials", "Finals", "Tones"];
 var MAX_LEVEL = [6,6,2,2,2];
-var VELOCITIES = [1,1.5,2,2.5,3]; 
+var VELOCITIES = [0.5,0.75,1,1.25,1.5]; 
 var LEVELBOUND = [-1,32,64,128,256,512,9999999];
 
 function getVelocity(mode, level){
--- msgbox.js
+++ msgbox.js
@@ -5,7 +5,7 @@
     
     this.x = 0;
     this.y = 240;
-    this.width = 800;
+    this.width = 260;
     this.height = 240;
     this.title = "";
     this.message = "";  
@@ -21,26 +21,26 @@
     this.message = message;
     
     box = new createjs.Shape();
-    box.graphics.setStrokeStyle(8,"round")
+    box.graphics.setStrokeStyle(4,"round")
         .beginStroke("#800000").beginFill('#CD853F')
         .drawRoundRectComplex (0, 0, this.width, this.height,10 , 10, 10, 10)
         .endFill();
     box.alpha = 1;
 
-    this.title_label = new createjs.Text(this.title, "bold 30px Pixel", "#FFFFAA");
-    this.title_label.maxWidth = 600;
+    this.title_label = new createjs.Text(this.title, "bold 20px Pixel", "#FFFFAA");
+    this.title_label.maxWidth = 240;
     this.title_label.textAlign = "left";
     this.title_label.textBaseline = "top";
-    this.title_label.x = 20;
-    this.title_label.y = 20;
+    this.title_label.x = 15;
+    this.title_label.y = 15;
     
-    this.message_label = new createjs.Text(this.message, "25px Pixel", "#FFFFFF");
-    this.message_label.maxWidth = 600;
+    this.message_label = new createjs.Text(this.message, "14px Pixel", "#FFFFFF");
+    this.message_label.maxWidth = 240;
     this.message_label.textAlign = "left";
     this.message_label.textBaseline = "top";
-    this.message_label.lineHeight = "35";
-    this.message_label.x = 20;
-    this.message_label.y = 70;    
+    this.message_label.lineHeight = "20";
+    this.message_label.x = 10;
+    this.message_label.y = 60;    
   };
 
   p.show = function(){
